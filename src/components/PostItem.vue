a198863d4273f0ebd5380cc95e70a31e30be39e6
Alex Ross <alexross1987@yahoo.co.uk>
2021-05-24 20:09:56 +0100

    Post Item component

---
+++ src/components/PostItem.vue
@@ -0,0 +1,37 @@
+<template v-if="post">
+    <div class="card mb-3">
+    <div class="card-body ">
+        <h5 class="card-title">{{post.title}}</h5>
+        <h6 v-if="authorPost" class="card-subtitle mb-2 text-muted">{{authorPost.name}}</h6>
+        <p class="card-text">{{post.body}}</p>
+        <PostComments :postId="post.id" />
+    </div>
+    </div>
+</template>
+
+<script>
+import PostComments from '@/components/PostComments.vue'
+
+export default {
+  name: 'PostItem',
+  components:{
+    PostComments
+  },
+  props: {
+    post: null,
+    authors: null,
+    author: Object
+  },
+  computed: {
+      authorPost() {
+          if (this.authors && this.post) {
+            return this.authors.filter((auth) => auth.id === this.post.userId)[0]
+          } 
+          return 'anonymous'
+      }
+  },
+  methods: {
+      
+  }
+}
+</script>

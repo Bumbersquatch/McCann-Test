2bf837f80b8b4b3c2336b0eddaa5f5bc1c94d5ba
Alex Ross <alexross1987@yahoo.co.uk>
2021-05-24 23:30:41 +0100

    About page

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import App from './App.vue'
 import router from './router'
 import './assets/styles/app.scss'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+.use(router)
+.mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>McCann Developer Test</h1>
+    <p>by Alex Ross</p>
+    <p>Find out more at <a href="https://rosso.codes" target="_blank" rel="noopener noreferrer">https://rosso.codes</a></p>
   </div>
 </template>
